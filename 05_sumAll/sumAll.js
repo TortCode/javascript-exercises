a6eabdaba41d0028eef9622d0218c37a14f6c73b
Teerth (Macbook) <Teerth Patel>
2024-11-24 22:29:09 -0600

    complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(start, stop) {
+    if (!Number.isInteger(start) || !Number.isInteger(stop) || start < 0 || stop < 0) {
+        return 'ERROR';
+    }
+    if (start > stop) {
+        [start, stop] = [stop, start];
+    }
+    return (start + stop) * (stop - start + 1) / 2;
 };
 
 // Do not edit below this line
