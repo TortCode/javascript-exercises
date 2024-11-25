33ae3568d15089be8b001375c064274073cdcc7c
Teerth (Macbook) <Teerth Patel>
2024-11-24 22:39:55 -0600

    complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const roundToOneDecimal = function(n) {
+  return Math.round(n * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToCelsius = function(f) {
+  return roundToOneDecimal((f - 32) * 5/9);
+};
+
+const convertToFahrenheit = function(c) {
+  return roundToOneDecimal(c * 9/5 + 32);
 };
 
 // Do not edit below this line
