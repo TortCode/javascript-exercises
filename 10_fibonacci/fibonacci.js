9cb48ba9d51dc25641243b7d604a54899dd46a4e
Teerth (Macbook) <Teerth Patel>
2025-01-22 16:56:09 -0600

    complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = parseInt(n);
+    if (n < 0) return "OOPS";
+    if (n === 0) return 0;
+    let a = 0;
+    let b = 1;
+    for (let i = 1; i < n; i++) {
+        let temp = a;
+        a = b;
+        b = temp + b;
+    }
+    return b;
 };
 
 // Do not edit below this line
