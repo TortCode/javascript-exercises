895e4563427e0b8c5378d5d8a993a5edc20c1c32
Teerth (Macbook) <Teerth Patel>
2025-01-22 17:02:55 -0600

    complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(persons) {
+    const calculateAge = function(person) {
+        const startYear = person.yearOfBirth;
+        const endYear = person.yearOfDeath ?? new Date().getFullYear();
+        const age = endYear - startYear; 
+        return age;
+    }
+    return persons.reduce((acc, person) => {
+        if (calculateAge(person) > calculateAge(acc)) {
+            return person;
+        } else {
+            return acc;
+        }
+    })
 };
 
 // Do not edit below this line
