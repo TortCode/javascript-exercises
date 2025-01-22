c14a167a24d54aae3d96f854bac679ad505d4204
Teerth (Macbook) <Teerth Patel>
2025-01-22 16:52:50 -0600

    complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+    const chars = s.toLowerCase().split('').filter(c => (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9'));
+    return chars.join('') === chars.toReversed().join('');
 };
 
 // Do not edit below this line
